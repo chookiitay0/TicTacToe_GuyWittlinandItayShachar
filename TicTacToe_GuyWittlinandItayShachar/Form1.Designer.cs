8e4335e76e479fa23dea51fcf729d8beecbb2ea4
גיא ויטלין <guy.vi2@tlvedu.org.il>
2020-12-13 12:19:13 +0200

    Add project files.

---
+++ TicTacToe_GuyWittlinandItayShachar/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace TicTacToe_GuyWittlinandItayShachar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Restat = new System.Windows.Forms.Button();
+            this.pictureBox22 = new System.Windows.Forms.PictureBox();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.pictureBox02 = new System.Windows.Forms.PictureBox();
+            this.pictureBox21 = new System.Windows.Forms.PictureBox();
+            this.pictureBox11 = new System.Windows.Forms.PictureBox();
+            this.pictureBox01 = new System.Windows.Forms.PictureBox();
+            this.pictureBox20 = new System.Windows.Forms.PictureBox();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox00 = new System.Windows.Forms.PictureBox();
+            this.BackPictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Timer_Text = new System.Windows.Forms.Label();
+            this.TheSeconds = new System.Windows.Forms.Label();
+            this.Turns = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Restat
+            // 
+            this.Restat.Location = new System.Drawing.Point(165, 361);
+            this.Restat.Name = "Restat";
+            this.Restat.Size = new System.Drawing.Size(169, 35);
+            this.Restat.TabIndex = 10;
+            this.Restat.Text = "Restart";
+            this.Restat.UseVisualStyleBackColor = true;
+            this.Restat.Click += new System.EventHandler(this.Restat_Click);
+            // 
+            // pictureBox22
+            // 
+            this.pictureBox22.Location = new System.Drawing.Point(339, 238);
+            this.pictureBox22.Name = "pictureBox22";
+            this.pictureBox22.Size = new System.Drawing.Size(169, 117);
+            this.pictureBox22.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox22.TabIndex = 9;
+            this.pictureBox22.TabStop = false;
+            this.pictureBox22.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::TicTacToe_GuyWittlinandItayShachar.Properties.Resources.None;
+            this.pictureBox12.Location = new System.Drawing.Point(338, 117);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(169, 116);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox12.TabIndex = 8;
+            this.pictureBox12.TabStop = false;
+            this.pictureBox12.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox02
+            // 
+            this.pictureBox02.Location = new System.Drawing.Point(339, 1);
+            this.pictureBox02.Name = "pictureBox02";
+            this.pictureBox02.Size = new System.Drawing.Size(169, 113);
+            this.pictureBox02.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox02.TabIndex = 7;
+            this.pictureBox02.TabStop = false;
+            this.pictureBox02.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox21
+            // 
+            this.pictureBox21.Location = new System.Drawing.Point(165, 237);
+            this.pictureBox21.Name = "pictureBox21";
+            this.pictureBox21.Size = new System.Drawing.Size(168, 117);
+            this.pictureBox21.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox21.TabIndex = 6;
+            this.pictureBox21.TabStop = false;
+            this.pictureBox21.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox11
+            // 
+            this.pictureBox11.Location = new System.Drawing.Point(165, 118);
+            this.pictureBox11.Name = "pictureBox11";
+            this.pictureBox11.Size = new System.Drawing.Size(168, 115);
+            this.pictureBox11.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox11.TabIndex = 5;
+            this.pictureBox11.TabStop = false;
+            this.pictureBox11.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox01
+            // 
+            this.pictureBox01.Image = global::TicTacToe_GuyWittlinandItayShachar.Properties.Resources.None;
+            this.pictureBox01.Location = new System.Drawing.Point(164, 2);
+            this.pictureBox01.Name = "pictureBox01";
+            this.pictureBox01.Size = new System.Drawing.Size(168, 112);
+            this.pictureBox01.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox01.TabIndex = 4;
+            this.pictureBox01.TabStop = false;
+            this.pictureBox01.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox20
+            // 
+            this.pictureBox20.Location = new System.Drawing.Point(-1, 239);
+            this.pictureBox20.Name = "pictureBox20";
+            this.pictureBox20.Size = new System.Drawing.Size(159, 113);
+            this.pictureBox20.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox20.TabIndex = 3;
+            this.pictureBox20.TabStop = false;
+            this.pictureBox20.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Location = new System.Drawing.Point(0, 119);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(159, 113);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 2;
+            this.pictureBox10.TabStop = false;
+            this.pictureBox10.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // pictureBox00
+            // 
+            this.pictureBox00.Image = global::TicTacToe_GuyWittlinandItayShachar.Properties.Resources.None;
+            this.pictureBox00.Location = new System.Drawing.Point(-1, 1);
+            this.pictureBox00.Name = "pictureBox00";
+            this.pictureBox00.Size = new System.Drawing.Size(159, 113);
+            this.pictureBox00.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox00.TabIndex = 1;
+            this.pictureBox00.TabStop = false;
+            this.pictureBox00.Click += new System.EventHandler(this.sign_Click);
+            // 
+            // BackPictureBox
+            // 
+            this.BackPictureBox.Image = global::TicTacToe_GuyWittlinandItayShachar.Properties.Resources.Back;
+            this.BackPictureBox.Location = new System.Drawing.Point(-1, 1);
+            this.BackPictureBox.Name = "BackPictureBox";
+            this.BackPictureBox.Size = new System.Drawing.Size(509, 354);
+            this.BackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackPictureBox.TabIndex = 0;
+            this.BackPictureBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Timer_Text
+            // 
+            this.Timer_Text.AutoSize = true;
+            this.Timer_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Timer_Text.Location = new System.Drawing.Point(-7, 354);
+            this.Timer_Text.Name = "Timer_Text";
+            this.Timer_Text.Size = new System.Drawing.Size(88, 31);
+            this.Timer_Text.TabIndex = 11;
+            this.Timer_Text.Text = "Timer";
+            // 
+            // TheSeconds
+            // 
+            this.TheSeconds.AutoSize = true;
+            this.TheSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.TheSeconds.Location = new System.Drawing.Point(11, 381);
+            this.TheSeconds.Name = "TheSeconds";
+            this.TheSeconds.Size = new System.Drawing.Size(123, 25);
+            this.TheSeconds.TabIndex = 12;
+            this.TheSeconds.Text = "0 Seconds";
+            this.TheSeconds.Click += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Turns
+            // 
+            this.Turns.AutoSize = true;
+            this.Turns.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.Turns.Location = new System.Drawing.Point(349, 367);
+            this.Turns.Name = "Turns";
+            this.Turns.Size = new System.Drawing.Size(146, 25);
+            this.Turns.TabIndex = 13;
+            this.Turns.Text = "X, Your Turn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 408);
+            this.Controls.Add(this.Turns);
+            this.Controls.Add(this.TheSeconds);
+            this.Controls.Add(this.Timer_Text);
+            this.Controls.Add(this.Restat);
+            this.Controls.Add(this.pictureBox22);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.pictureBox02);
+            this.Controls.Add(this.pictureBox21);
+            this.Controls.Add(this.pictureBox11);
+            this.Controls.Add(this.pictureBox01);
+            this.Controls.Add(this.pictureBox20);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox00);
+            this.Controls.Add(this.BackPictureBox);
+            this.Name = "Form1";
+            this.Text = "TicTacToe - Guy Wittlin and Itay Shachar";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox22)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox02)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox11)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox20)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox00)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BackPictureBox;
+        private System.Windows.Forms.PictureBox pictureBox00;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.PictureBox pictureBox20;
+        private System.Windows.Forms.PictureBox pictureBox21;
+        private System.Windows.Forms.PictureBox pictureBox11;
+        private System.Windows.Forms.PictureBox pictureBox01;
+        private System.Windows.Forms.PictureBox pictureBox22;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.PictureBox pictureBox02;
+        private System.Windows.Forms.Button Restat;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Timer_Text;
+        private System.Windows.Forms.Label TheSeconds;
+        private System.Windows.Forms.Label Turns;
+    }
+}
+
---
+++ TicTacToe_GuyWittlinandItayShachar/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TicTacToe_GuyWittlinandItayShachar.Properties;
+
+namespace TicTacToe_GuyWittlinandItayShachar
+{
+    public partial class Form1 : Form
+    {
+        private bool m_IsFirstPlayer = true;
+        private int tick;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        public bool IsImagesMatch(Image image1, Image image2)
+        {
+            try
+            {
+                //create instance or System.Drawing.ImageConverter to convert
+                //each image to a byte array
+                ImageConverter converter = new ImageConverter();
+                //create 2 byte arrays, one for each image
+                byte[] imgBytes1 = new byte[1];
+                byte[] imgBytes2 = new byte[1];
+
+                //convert images to byte array
+                imgBytes1 = (byte[])converter.ConvertTo(image1, imgBytes2.GetType());
+                imgBytes2 = (byte[])converter.ConvertTo(image2, imgBytes1.GetType());
+
+                //now compute a hash for each image from the byte arrays
+                System.Security.Cryptography.SHA256Managed sha = new System.Security.Cryptography.SHA256Managed();
+                byte[] imgHash1 = sha.ComputeHash(imgBytes1);
+                byte[] imgHash2 = sha.ComputeHash(imgBytes2);
+
+                //now let's compare the hashes
+                for (int i = 0; i < imgHash1.Length && i < imgHash2.Length; i++)
+                {
+                    //whoops, found a non-match, exit the loop
+                    //with a false value
+                    if (!(imgHash1[i] == imgHash2[i]))
+                        return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            //we made it this far so the images must match
+            return true;
+        }
+        public string[,] FormToMatrix()
+        {
+            //המטריצה המוחזרת
+            string[,] picturesMatrix = new string[3, 3];
+            int row, col;
+            PictureBox curPicture;
+
+            //עוברים על כלל הפקדים בטופס - נעזרים בלולאת foreach
+            foreach (Control ctrl in this.Controls)
+                if (ctrl is PictureBox && ctrl.Name != "BackPictureBox")
+                {
+                    curPicture = ctrl as PictureBox;
+
+                    //מציאת מספר השורה והעמודה מתוך שם הפקד
+                    row = int.Parse(curPicture.Name[curPicture.Name.Length - 2].ToString());
+                    col = int.Parse(curPicture.Name[curPicture.Name.Length - 1].ToString());
+                    picturesMatrix[row, col] = ImgToXO(curPicture.Image);
+                }
+            return picturesMatrix;
+        }
+        public string ImgToXO(Image img)
+        {
+            if (IsImagesMatch(img, Resources.X_Button))
+                return "X";
+            if (IsImagesMatch(img, Resources.O_Button))
+                return "0";
+            return "";
+        }
+        public bool CheckMatrix(string[,] S_Matrix, string s)
+        {
+            int countR = 0, countH = 0, countRD = 0, countSD = 0;
+            for (int i = 0; i < S_Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < S_Matrix.GetLength(1); j++)
+                {
+                    if (S_Matrix[i, j] == s)
+                    {
+                        countR++;
+                    }
+                    if (countR == 3)
+                    {
+                        return true;
+                    }
+                }
+                countR = 0;
+            }
+            for (int i = 0; i < S_Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < S_Matrix.GetLength(1); j++)
+                {
+                    if (S_Matrix[j, i] == s)
+                    {
+                        countH++;
+                    }
+                    if (countH == 3)
+                    {
+                        return true;
+                    }
+                }
+                countH = 0;
+            }
+            for (int i = 0; i < S_Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < S_Matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        if (S_Matrix[i, j] == s)
+                        {
+                            countRD++;
+                        }
+                        if (countRD == 3)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < S_Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < S_Matrix.GetLength(1); j++)
+                {
+                    if ((i + j) == (S_Matrix.GetLength(0) - 1))
+                    {
+                        if (S_Matrix[i, j] == s)
+                        {
+                            countSD++;
+                        }
+                    }
+                    if (countSD == 3)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;         
+        }
+        private void sign_Click(object sender, EventArgs e)
+        {
+            int drawcount = 0;
+            PictureBox sign = sender as PictureBox;
+
+            if (m_IsFirstPlayer)
+            {
+                Turns.Text = "0, Your Turn";
+                sign.Image = Resources.X_Button;
+                m_IsFirstPlayer = false;
+                sign.Enabled = false;
+                if (CheckMatrix(FormToMatrix(), "X"))
+                {
+                    MessageBox.Show("X, You Win");
+                    timer1.Stop();
+                }
+            }
+            else
+            {
+                Turns.Text = "X, Your Turn";
+                sign.Image = Resources.O_Button;
+                m_IsFirstPlayer = true;
+                sign.Enabled = false;
+                if (CheckMatrix(FormToMatrix(), "0"))
+                {
+                    MessageBox.Show("0, You Win");
+                    timer1.Stop();
+                }
+            }
+        }
+        private void Restat_Click(object sender, EventArgs e)
+        {
+            m_IsFirstPlayer = true;
+            Turns.Text = "X, Your Turn";
+            pictureBox00.Image = Resources.None;
+            pictureBox00.Enabled = true;
+            pictureBox01.Image = Resources.None;
+            pictureBox01.Enabled = true;
+            pictureBox02.Image = Resources.None;
+            pictureBox02.Enabled = true;
+
+            pictureBox10.Image = Resources.None;
+            pictureBox10.Enabled = true; 
+            pictureBox11.Image = Resources.None;
+            pictureBox11.Enabled = true; 
+            pictureBox12.Image = Resources.None;
+            pictureBox12.Enabled = true;
+
+            pictureBox20.Image = Resources.None;
+            pictureBox20.Enabled = true;
+            pictureBox21.Image = Resources.None;
+            pictureBox21.Enabled = true;
+            pictureBox22.Image = Resources.None;
+            pictureBox22.Enabled = true;
+
+            tick = 0;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            tick++;
+            TheSeconds.Text = tick.ToString() + " Seconds";
+        }
+    }
+}
